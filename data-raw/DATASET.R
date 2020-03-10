79b126d77eebc918b19b25745d8bb81b273b620a
pcote <pcote@ualberta.ca>
2020-03-10 11:29:54 -0600

    2020 03 10 expiry_table CL v2

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -38,22 +38,17 @@ usethis::use_data(tickers_eia, overwrite = T)
   # write_csv2(expiries,"~/nymex_wti_expiries.csv")
 
 futmonths = c("F","G","H","J","K","M","N","Q","U","V","X","Z")
-expiry_table<-read.csv('expiry_table.csv',sep=",",header=TRUE,na.strings="NA",stringsAsFactors=FALSE)
+expiry_table <- read.csv('expiry_table.csv',sep=",",header=TRUE,na.strings="NA",stringsAsFactors=FALSE)
 expiry_table <- dplyr::mutate(expiry_table,
-                              Last.Trade = as.Date(as.character(Last.Trade),"%Y/%m/%d",tz="UTC"),
-                              First.Notice = as.Date(as.character(First.Notice),"%Y/%m/%d",tz="UTC"),
-                              First.Delivery = as.Date(as.character(First.Delivery),"%Y/%m/%d",tz="UTC"),
-                              Last.Delivery = as.Date(as.character(Last.Delivery),"%Y/%m/%d",tz="UTC"),
+                              Last.Trade = as.Date(as.character(Last.Trade),"%Y-%m-%d",tz="UTC"),
+                              First.Notice = as.Date(as.character(First.Notice),"%Y-%m-%d",tz="UTC"),
+                              First.Delivery = as.Date(as.character(First.Delivery),"%Y-%m-%d",tz="UTC"),
+                              Last.Delivery = as.Date(as.character(Last.Delivery),"%Y-%m-%d",tz="UTC"),
                               Year = year(First.Delivery),
                               Month = month(First.Delivery),
                               Month.Letter = futmonths[Month],
-                              DataStream = case_when(cmdty=="cmewti" ~ paste("NCL",sprintf('%0.2d',Month),sprintf('%0.2d',Year-2000),sep=""),
-                                                     cmdty=="cmeng" ~ paste("NNG",sprintf('%0.2d',Month),sprintf('%0.2d',Year-2000),sep=""),
-                                                     cmdty=="icebrent" ~ paste("LLC",sprintf('%0.2d',Month),sprintf('%0.2d',Year-2000),sep=""),
-                                                     cmdty=="cmeulsd" ~ paste("NHO",sprintf('%0.2d',Month),sprintf('%0.2d',Year-2000),sep=""),
-                                                     cmdty=="cmerbob" ~ paste("NRB",sprintf('%0.2d',Month),sprintf('%0.2d',Year-2000),sep="")),
-                              ticker = paste(tick.prefix,"_",Year,Month.Letter,sep="")
-) %>% dplyr::filter(Year>2003)
+                              ticker = paste(tick.prefix,"_",Year,Month.Letter,sep="")) %>%
+  dplyr::filter(Year > 2003)
 usethis::use_data(expiry_table, overwrite = T)
 
 ## Holiday Calendar

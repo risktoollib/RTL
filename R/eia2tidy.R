ff6472b1dd07b3fbf8a7607236adcc4c605e402f
risktoollib <pcote@ualberta.ca>
2020-09-19 05:34:00 -0600

    2020 09 19 eia2tidy doc

--- R/eia2tidy.R
+++ R/eia2tidy.R
@@ -2,7 +2,7 @@
 #' @description Extracts data from the Energy Information Administration (EIA) API to tibble format with optional custom series name.
 #' Makes a clean wrapper for use with purrr for multiple series extraction. Query Browser at https://www.eia.gov/opendata/qb.php.
 #' @param ticker EIA series name.
-#' @param key Your private EIA API token.
+#' @param key Your private EIA API token as character "yourkey".
 #' @param name Name you want to give the series. Defaults to ticker if set to " "
 #' @return A tibble object with class date for weekly, monthly, quarterly or annual data and class POSIXct for hourly.
 #' @export eia2tidy
@@ -15,7 +15,7 @@
 #' eia_df <-tibble::tribble(~ticker, ~name,
 #' "PET.W_EPC0_SAX_YCUOK_MBBL.W", "CrudeCushing",
 #' "NG.NW2_EPG0_SWO_R48_BCF.W","NGLower48") %>%
-#'  dplyr::mutate(key = EIAkey) %>%
+#'  dplyr::mutate(key = "EIAkey") %>%
 #'  dplyr::mutate(df = purrr::pmap(list(ticker,key,name),.f=RTL::eia2tidy)) %>%
 #'  dplyr::select(df) %>% tidyr::unnest(df)
 #'  }

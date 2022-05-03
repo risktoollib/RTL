f5c5fa5c2014408ad757f9b91fbef204c18570b4
risktoollib <pcote@ualberta.ca>
2022-05-03 14:36:10 -0600

    simGBM vectorized

--- R/simGBM.R
+++ R/simGBM.R
@@ -1,23 +1,32 @@
 #' GBM process simulation
 #' @description Simulates a Geometric Brownian Motion process
+#' @param nsims numble of simulations. Defaults to 1
 #' @param S0 Spot price at t=0
 #' @param drift Drift term in percentage
 #' @param sigma Standard deviation
 #' @param T2M Maturity in years
 #' @param dt Time step in period e.g. 1/250 = 1 business day.
-#' @return A numeric vector of simulated values
+#' @param vec Vectorized implementation. Defaults to TRUE
+#' @return A tibble of simulated values
 #' @export simGBM
 #' @author Philippe Cote
 #' @examples
-#' simGBM(S0 = 10, drift = 0, sigma = 0.2, T2M = 1, dt = 1 / 12)
-simGBM <- function(S0 = 10, drift = 0, sigma = 0.2, T2M = 1, dt = 1 / 12) {
+#' simGBM(nsims = 2, S0 = 10, drift = 0, sigma = 0.2, T2M = 1, dt = 1 / 12, vec = TRUE)
+simGBM <- function(nsims = 1, S0 = 10, drift = 0, sigma = 0.2, T2M = 1, dt = 1 / 12, vec = TRUE) {
   periods <- T2M / dt
-  S <- rep(S0, periods)
-  for (i in 2:periods) {
-    S[i] <- S[i - 1] * exp(
-      (drift - (sigma^2) / 2) * dt +
-        sigma * stats::rnorm(1, mean = 0, sd = 1) * sqrt(dt)
-    )
+  if (vec == FALSE & nsims > 1) stop("For nsmis > 1, use set vec to TRUE. Non-vectorize version only shown for education purposes.")
+  if (vec == FALSE) {
+    S <- rep(S0, periods + 1)
+    for (i in 2:(periods + 1)) {
+      S[i] <- S[i - 1] * exp((drift - (sigma^2) / 2) * dt + sigma * stats::rnorm(1, mean = 0, sd = sqrt(dt)))
+    }
+  } else {
+    diffusion <- NULL
+    diffusion <- matrix(stats::rnorm(periods * nsims, mean = 0, sd = sqrt(dt)), ncol = nsims, nrow = periods)
+    S <- exp((drift - (sigma^2) / 2) * dt + sigma * diffusion)
+    S <- apply(rbind(rep(S0,nsims),S),2,cumprod)
+    S <- dplyr::as_tibble(S,.name_repair = "universal")
+    names(S) <- paste("sim",1:nsims,sep="")
   }
   return(S)
 }
